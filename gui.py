a4d8874dfa8eebf74e539f2d4fe3254068e6da02
Adrian Jaśkowiec <ajaskowiec@onet.eu>
2023-05-07 13:55:34 +0200

    a

--- gui.py
+++ gui.py
@@ -1,4 +1,9 @@
 import PySimpleGUI as sg
+from antlr4 import CommonTokenStream
+
+from logo.LogoLexer import LogoLexer
+from logo.LogoParser import LogoParser
+from main import MyVisitor, Turtle
 
 size = 400
 
@@ -8,11 +13,33 @@ layout = [[sg.Canvas(size=(size, size), key='-CANVAS-')],
 
 window = sg.Window('PyTurtle', layout)
 
+turtle = Turtle(size//2, size//2)
+variables = {}
+local_variables = None
+functions = {}
+
+# Show turtle
+event, values = window.read()
+window['-CANVAS-'].TKCanvas.create_oval(turtle.x - 5, turtle.y - 5, turtle.x + 5, turtle.y + 5, fill='black')
+
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    elif event == 'Draw':
+    elif event == 'Push':
         canvas = window['-CANVAS-'].TKCanvas
-        canvas.create_oval(size // 2 - 5, size // 2 - 5, size // 2 + 5, size // 2 + 5, fill='black')
+        data = values['-INPUT-']
+
+        # lexer
+        lexer = LogoLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = LogoParser(stream)
+
+        tree = parser.program()
+        # evaluator
+        visitor = MyVisitor()
+        output = visitor.visit(tree)
+        # print(output)
+        canvas.create_oval(turtle.x - 5, turtle.y - 5, turtle.x + 5, turtle.y + 5, fill='black')
 window.close()
