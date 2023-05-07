c497a2722d379aa1e4572ba203e6fe4c4eb8dbdf
Adrian Jaśkowiec <ajaskowiec@onet.eu>
2023-05-07 14:00:46 +0200

    a

--- gui.py
+++ gui.py
@@ -11,23 +11,26 @@ layout = [[sg.Canvas(size=(size, size), key='-CANVAS-')],
           [sg.InputText(key='-INPUT-'), sg.Button('Push'), sg.Button('Exit')],
           ]
 
-window = sg.Window('PyTurtle', layout)
+window = sg.Window('PyTurtle', layout, finalize=True)
 
 turtle = Turtle(size//2, size//2)
 variables = {}
 local_variables = None
 functions = {}
 
+canvas = window['-CANVAS-'].TKCanvas
+
 # Show turtle
-event, values = window.read()
-window['-CANVAS-'].TKCanvas.create_oval(turtle.x - 5, turtle.y - 5, turtle.x + 5, turtle.y + 5, fill='black')
+# event, values = window.read()
+
+canvas.create_oval(turtle.x - 5, turtle.y - 5, turtle.x + 5, turtle.y + 5, fill='black')
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     elif event == 'Push':
-        canvas = window['-CANVAS-'].TKCanvas
+        # canvas = window['-CANVAS-'].TKCanvas
         data = values['-INPUT-']
 
         # lexer
